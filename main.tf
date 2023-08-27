720b3bf8eceabb5d48e521d3587aa99727842627
NikhilPatil333 <nikhilpatil30397@gmail.com>
2023-08-27 16:58:01 +0530

    main / providers

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-  access_key = "ASIAZ627RA3QO2KEVRKB"	
-  secret_key = "xo+v1ZZbMVDQAWYt7VRhV8PP+nQOWo4sIeLO0VJz"
-  
-}
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "terraform-nikhil-prac"
